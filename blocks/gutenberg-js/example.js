0d5b71553f3d6e66b73f2d506df6ed606b0063e1
yfilipenko <79315246+yfilipenko@users.noreply.github.com>
2021-07-01 10:12:08 +0300

    Create example.js

---
+++ blocks/gutenberg-js/example.js
@@ -0,0 +1,25 @@
+(function($){
+
+    /**
+     * initializeBlock
+     *
+     * Adds custom JavaScript to the block HTML.
+     *
+     * @date    15/4/19
+     * @since   1.0.0
+     *
+     * @param   object $block The block jQuery element.
+     * @param   object attributes The block attributes (only available when editing).
+     * @return  void
+     */
+    var initializeBlock = function( $block ) {
+        //Your js code
+    }
+    
+
+    // Initialize dynamic block preview (editor).
+    if( window.acf ) {
+        window.acf.addAction( 'render_block_preview/type=example', initializeBlock );
+    }
+
+})(jQuery);
