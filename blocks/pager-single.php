decb057ce880f129d417e2f8673b4d6ceb5d75ab
yfilipenko <79315246+yfilipenko@users.noreply.github.com>
2021-11-23 13:32:37 +0300

    Arrow fix

--- blocks/pager-single.php
+++ blocks/pager-single.php
@@ -2,7 +2,7 @@
 <?php $next = get_next_post_link( __( '%link', get_option('stylesheet') ), '%title&nbsp;&raquo;' ) ?>
 <?php if( $prev || $next ) : ?>
 	<div class="navigation-single">
-		<?php if( $next ) : ?><div class="next"><?php echo $next ?></div><?php endif ?>
 		<?php if( $prev ) : ?><div class="prev"><?php echo $prev ?></div><?php endif ?>
+		<?php if( $next ) : ?><div class="next"><?php echo $next ?></div><?php endif ?>
 	</div>
 <?php endif ?>
